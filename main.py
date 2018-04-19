cbd2d98257535574cc51cf48eeb47aaf57ed87de
David Bell <david.a.bell@uwaterloo.ca>
2018-04-19 15:51:36 -0400

    Rough layout UI

--- main.py
+++ main.py
@@ -2,12 +2,22 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.label import Label
 
+class DistanceElement(Widget):
+    pass
+
+class ODOElement(Widget):
+    pass
+
+class TestElement(Widget):
+    pass
+
 class TestScreen(Widget):
     pass
 
 class NavODOApp(App):
     def build(self):
-        return Label(text='Hello world')
+        # return Label(text='Hello world')
+        return TestScreen()
 
 
 if __name__ == '__main__':
