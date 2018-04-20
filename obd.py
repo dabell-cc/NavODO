0438bf69d07ae33719876a4c1390b4e82d46dc13
David Bell <david.antony.bell@gmail.com>
2018-04-20 00:02:54 -0400

    obd cmd test common commands

---
+++ obd.py
@@ -0,0 +1,20 @@
+import obd
+from obd import ECU
+
+ports = obd.scan_serial()
+
+print(ports)
+obd.logger.setLevel(obd.logging.DEBUG)
+connection = obd.OBD(portstr=ports[2],fast=False)
+
+cmd = obd.commands.RPM.ecu = ECU.ALL
+resp = connection.query(cmd)
+print(resp.value)
+
+cmd = obd.commands.PIDS_A.ecu = ECU.ALL
+resp = connection.query(cmd)
+print(resp.value)
+
+cmd = obd.commands.PIDS_B.ecu = ECU.ALL
+resp = connection.query(cmd)
+print(resp.value)
