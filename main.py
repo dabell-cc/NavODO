dc020f13ec49f7bd83f24fc44b50ae713abdd64f
David Bell <dabell@uwaterloo.ca>
2018-04-20 17:56:50 -0400

    adding stopwatch/clock

--- main.py
+++ main.py
@@ -1,24 +1,76 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
-from kivy.properties import NumericProperty
+from kivy.properties import NumericProperty,StringProperty, \
+    BooleanProperty
+from kivy.clock import Clock
+import time
+from time import monotonic as timer
+from datetime import timedelta 
 
-class DistanceElement(Widget):
-    pass
-
-class ODOElement(Widget):
-    pass
-
-class TestElement(Widget):
-    pass
 
 class TestScreen(Widget):
+    # Timing and Clock
+    clock_timeofday = StringProperty()
+    stopwatch_dt = StringProperty('')
+    stopwatch_start = 0
+    stopwatch_stop = 0
+    stopwatch_is_running = BooleanProperty(False)
+
+    # Speeds
+    speed_CAS = NumericProperty(0)
+    speed_Curr = NumericProperty(0)
+
+    # Distances
+    distance_incODO = NumericProperty(0)
+    distance_stageODO = NumericProperty(0)
+
+    def test_callback(self):
+        print('button push')
+
+    def zero_CAS(self):
+        pass
     
-    pass
+    def zero_stageODO(self):
+        pass
+    
+    def zero_incODO(self):
+        pass
+    
+    def reset_speed(self):
+        pass
+    
+    def stopwatch_start_stop(self):
+        if(self.stopwatch_is_running):
+            self.stopwatch_stop = timer()
+            self.stopwatch_is_running = False
+        else:
+            self.stopwatch_start = timer()
+            self.stopwatch_is_running = True
+    
+    def stopwatch_reset(self):
+        self.stopwatch_start = timer()
+        self.stopwatch_start = self.stopwatch_stop
+    
+
+    def update(self, dt):
+        # Update the Clock
+        self.clock_timeofday = time.strftime('%H : %M : %S')
+        
+        # Update the stopwatch, if it's running
+        if(self.stopwatch_is_running):
+            self.stopwatch_stop = timer()
+        
+        self.stopwatch_dt = str(timedelta(seconds=self.stopwatch_stop-self.stopwatch_start))
+        
+
+
 
 class NavODOApp(App):
     def build(self):
         # return Label(text='Hello world')
-        return TestScreen()
+        app = TestScreen()
+        Clock.schedule_interval(app.update, 1.0/3.0)
+        return app
 
 
 if __name__ == '__main__':
